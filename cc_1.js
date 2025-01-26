6cff81a818c5d521ff1d72cbb0e891b512177ad2
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-01-26 12:49:10 -0500

    Task 1 employee information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+let employeeName = "Francis Pablo";
+const employeeID = 230;
+var isActive = true;
+
+console.log("Employee Name:" ,employeeName, "Type:" , typeof employeeName);
+console.log("ID:" , employeeID, "Type:" , typeof employeeID);
+console.log("Status:", isActive, "Type:", typeof isActive);
+//Task 1 Employee Information
