3e25cd8c281b62c78ccb628f2d6e1447edc66975
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-01-26 13:07:01 -0500

    task 3 financial transaction

--- cc_1.js
+++ cc_1.js
@@ -15,3 +15,20 @@ console.log("Product:" ,productName, "Type:" , typeof productName);
 console.log("Price:" , productPrice, "Type:" , typeof productPrice);
 console.log("Availabilty:", isAvailable, "Type:", typeof isAvailable);
 //"Task 2 - Product details"
+
+let accountBalance = 1000;
+
+accountBalance += 100; // Addition
+console.log("After Selling Ps4:", accountBalance);
+
+accountBalance -= 350; //subtraction 
+console.log ("After purchasing Ps5:", accountBalance);
+
+accountBalance *= 1.20; // 20% interest
+console.log ("Bank Interest:", accountBalance);
+
+accountBalance /= 1.75; //division
+console.log ("After taxes:", accountBalance);
+
+//Task # 3 financial transaction
+
