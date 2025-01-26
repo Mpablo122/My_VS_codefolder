edf54e98d25b86a9ca27eca9eff51d47d4dc3bd7
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-01-26 12:53:27 -0500

    Task 2 Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,4 +5,13 @@ var isActive = true;
 console.log("Employee Name:" ,employeeName, "Type:" , typeof employeeName);
 console.log("ID:" , employeeID, "Type:" , typeof employeeID);
 console.log("Status:", isActive, "Type:", typeof isActive);
-//Task 1 Employee Information
+//"Task 1 Employee Information"
+
+let productName = twix;
+const productPrice = 1.99;
+var isAvailable = true;
+
+console.log("Product:" ,productName, "Type:" , typeof productName);
+console.log("Price:" , productPrice, "Type:" , typeof productPrice);
+console.log("Availabilty:", isAvailable, "Type:", typeof isAvailable);
+//"Task 2 - Product details"
